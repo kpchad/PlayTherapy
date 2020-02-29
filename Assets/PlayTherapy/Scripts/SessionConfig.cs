4dbeefeb3fa3219a93a6155448db8d81f91c8865
kpchad <kylepchad@gmail.com>
2020-02-28 22:44:50 -0800

    config scene progress

---
+++ Assets/PlayTherapy/Scripts/SessionConfig.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SessionConfig : MonoBehaviour
+{
+
+    enum exercise { tabletop, knucklebend };
+    enum difficulty { easy, medium, hard};
+    enum side { left, right, both};
+    enum theme {tomsawyer, tswift};
+
+    void createSession(exercise exercise1, exercise exercise2, difficulty difficulty, side side, theme theme)
+    {
+        
+    }
+
+
+}
